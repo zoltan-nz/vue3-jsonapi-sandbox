f2da085a3d13ff4e2978a72b2a36758d505fc8f0
Zoltan <zdebre@gmail.com>
2023-05-07 23:16:57 -0400

    Clean up and deploy to fly.io

---
+++ frontend/src/app.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from '@/components/hello-world.vue'
+</script>
+
+<template>
+  <header>
+    <div>
+      <HelloWorld />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink> |
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
---
+++ frontend/src/pages/about-page.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <h1>About Page</h1>
+  </main>
+</template>
